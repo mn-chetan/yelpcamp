c0ee8cb275f3e37861b5b4b4cfe9ccc2e122179c
mn-chetan <165123379+mn-chetan@users.noreply.github.com>
2025-04-17 00:00:29 +0530

    Set up MongoDB connection and basic campground route
    Adds mongoose dependency and establishes a connection to the MongoDB database. Includes basic error handling and connection logging.
    
    Also adds an initial placeholder route for `/campground`. This route is currently incomplete and will be fully implemented tomorrow.

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const Campground = require("./models/campground");
+
+mongoose.connect("mongodb://127.0.0.1:27017/yelp-camp").catch((error) => {
+  console.log(error);
+});
+
+mongoose.connection.on("error", (err) => {
+  console.error(err);
+});
+mongoose.connection.on("connected", () => {
+  console.log("Connected to DB");
+});
 
 app.listen(3000, () => {
   console.log("Listening on port 3000");
 });
+
+app.get("/campground");
