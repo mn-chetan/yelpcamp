bcc9222bcdb4f74a6950b4a2d93cd16728dc12b8
mn-chetan <165123379+mn-chetan@users.noreply.github.com>
2025-04-17 17:06:51 +0530

    Add campground creation functionality
    Adds routes and views necessary to create new campgrounds.
    
    - Implement GET /campgrounds/new route to render the new campground form.
    - Implement POST /campgrounds route to handle form submission, create a new campground record in the database, and redirect to the show page for the newly created campground.
    - Add body-parser middleware to parse incoming request bodies.
    - Refactor Express app initialization slightly.

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-const express = require("express");
-const app = express();
+const app = require("express")();
+const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const Campground = require("./models/campground");
 const path = require("path");
 
+// Mongoose Connection
 mongoose.connect("mongodb://127.0.0.1:27017/yelp-camp").catch((error) => {
   console.log(error);
 });
@@ -15,8 +16,11 @@ mongoose.connection.on("connected", () => {
   console.log("Connected to DB");
 });
 
+// Express routing
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(3000, () => {
   console.log("Listening on port 3000");
@@ -29,6 +33,15 @@ app.get("/", (req, res) => {
 app.get("/campgrounds", async (req, res) => {
   const campgrounds = await Campground.find({});
   res.render("campgrounds/index", { campgrounds });
+});
+
+app.get("/campgrounds/new", (req, res) => {
+  res.render("campgrounds/new");
+});
+
+app.post("/campgrounds", async (req, res) => {
+  const campground = await Campground.create(req.body);
+  res.redirect(`/campgrounds/${campground._id}`);
 });
 
 app.get("/campgrounds/:id", async (req, res) => {
