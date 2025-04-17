111e10c8fcccda99a547ab5550f96ffa033b0b62
mn-chetan <165123379+mn-chetan@users.noreply.github.com>
2025-04-17 12:37:44 +0530

    Set up view engine and define routes for home and campgrounds

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const Campground = require("./models/campground");
+const path = require("path");
 
 mongoose.connect("mongodb://127.0.0.1:27017/yelp-camp").catch((error) => {
   console.log(error);
@@ -14,8 +15,18 @@ mongoose.connection.on("connected", () => {
   console.log("Connected to DB");
 });
 
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
 app.listen(3000, () => {
   console.log("Listening on port 3000");
 });
 
-app.get("/campground");
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/campgrounds", async (req, res) => {
+  const campgrounds = await Campground.find({});
+  res.render("campgrounds/index", { campgrounds });
+});
